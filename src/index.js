b02e4d8f1da6097d0bae4c8d9f765521c8013b83
BL-CKPD <chandrakant.prasad@bridgelabz.com>
2024-07-27 21:13:52 +0530

    [Chandss] ADD: Added vercel

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ import logger, { logStream } from './config/logger';
 import morgan from 'morgan';
 
 const app = express();
-const host = process.env.APP_HOST;
-const port = process.env.APP_PORT || 5001;
-const api_version = process.env.API_VERSION;
+// const host = process.env.APP_HOST || 'http://localhost';
+const port = process.env.APP_PORT || 3000;
+const api_version = process.env.API_VERSION || 'v1';
 
 app.use(cors());
 app.use(helmet());
@@ -38,7 +38,8 @@ app.use(genericErrorHandler);
 app.use(notFound);
 
 app.listen(port, () => {
-  logger.info(`Server started at ${host}:${port}/api/${api_version}/`);
+  // logger.info(`Server started at ${host}:${port}/api/${api_version}/`);
+  logger.info(`Server started at on vercel:${port}/api/${api_version}/`);
 });
 
 export default app;
